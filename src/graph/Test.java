85c7642999cb9aa8f06a26f2187e86385f23db8f
arpitbhargava <arpit.iitdhn@gmail.com>
2021-08-14 15:57:42 +0530

    New commit

--- src/graph/Test.java
+++ src/graph/Test.java
@@ -2,18 +2,21 @@ package graph;
 
 public class Test {
     public static void main(String[] args) {
-        Graph g=new Graph(6);
-        g.addEdges(0,1);
-        g.addEdges(0,2);
-        g.addEdges(2,1);
-        g.addEdges(3,1);
-        g.addEdges(4,3);
-        g.addEdges(5,0);
-       // Graph.printGraph(g);
+        Graph g=new Graph(7);
+        g.addEdges(1,2);
+        g.addEdges(1,0);
+        g.addEdges(2,3);
+        g.addEdges(0,4);
+        g.addEdges(3,4);
+        g.addEdges(3,5);
+        g.addEdges(4,5);
+        g.addEdges(5,6);
+        //Graph.printAdjacentList(g);
 
+        Graph.bfs(g,1,6);
        // int[][] edges={{0,1},{0,2},{0,3},{0,4},{0,5}};
-        Graph g1=new Graph(6);
-        Graph.printAdjacentMatrix(g1);
+       // Graph g1=new Graph(6);
+       // Graph.printAdjacentMatrix(g1);
 
 
 
